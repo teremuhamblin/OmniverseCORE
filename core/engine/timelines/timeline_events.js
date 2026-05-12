f6d2404b9afea104974f9f03ff47894cf8e5224f
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-12 09:29:16 +0200

    Create timeline_events.js

---
+++ core/engine/timelines/timeline_events.js
@@ -0,0 +1,4 @@
+[
+  { "id": "boot", "time": 0, "type": "system", "description": "OmniverseCORE initialized" },
+  { "id": "stabilize", "time": 5, "type": "engine", "description": "Stability matrix calibrated" }
+]
