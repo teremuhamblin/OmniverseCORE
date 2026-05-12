c9b2157cf348d506b8d541b57f700460c742470b
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-12 12:54:25 +0200

    Create Home.tsx

---
+++ dashboard/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import StatsPanel from "../components/StatsPanel";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-2xl mb-4">Home</h2>
+      <StatsPanel />
+    </div>
+  );
+}
