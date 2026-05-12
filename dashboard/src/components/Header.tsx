a0f6662dc3e375731bc8143d16599b4859a61381
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-12 12:51:57 +0200

    Create Header.tsx

---
+++ dashboard/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="w-full bg-black/30 border-b border-neon p-4">
+      <h1 className="text-2xl">Dashboard OmniverseCORE</h1>
+    </div>
+  );
+}
