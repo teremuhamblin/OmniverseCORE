3af9a81a088dfe1d7d2b150639ac1ac7481f49db
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-13 22:14:25 +0200

    Create docs.php

---
+++ siteweb/app/views/docs.php
@@ -0,0 +1,9 @@
+<section class="docs">
+    <h1>Documentation</h1>
+
+    <ul>
+        <?php foreach ($sections as $section): ?>
+            <li><?= $section ?></li>
+        <?php endforeach; ?>
+    </ul>
+</section>
