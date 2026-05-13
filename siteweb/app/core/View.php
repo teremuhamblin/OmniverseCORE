f8fbd52b84fb0c4fd1733dbbbba5fbd4b2e1aa67
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-13 22:38:16 +0200

    Update View.php

--- siteweb/app/core/View.php
+++ siteweb/app/core/View.php
@@ -1,8 +1,20 @@
 <?php
 
-class View {
-    public static function render(string $template, array $data = []) {
+namespace App\Core;
+
+class View
+{
+    public static function render(string $template, array $data = []): void
+    {
         extract($data);
-        require __DIR__ . '/../views/layout.php';
+        $templateFile = __DIR__ . '/../views/' . $template . '.php';
+        $layoutFile   = __DIR__ . '/../views/layout.php';
+
+        if (!file_exists($templateFile)) {
+            throw new \RuntimeException("Vue introuvable : $templateFile");
+        }
+
+        $contentView = $templateFile;
+        require $layoutFile;
     }
 }
