a12ab3fb993e2ecef797d6ea96e4e4337e365f5f
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-13 22:40:23 +0200

    Update routes.php

--- siteweb/app/routes.php
+++ siteweb/app/routes.php
@@ -1,4 +1,13 @@
 <?php
 
-$router->get('/', [new HomeController, 'index']);
-$router->get('/docs', [new DocsController, 'index']);
+use App\Controllers\HomeController;
+use App\Controllers\DocsController;
+use App\Controllers\ApiController;
+
+/** @var \App\Core\Router $router */
+
+$router->get('/', [HomeController::class, 'index']);
+$router->get('/docs', [DocsController::class, 'index']);
+
+// API simple
+$router->get('/api/status', [ApiController::class, 'status']);
