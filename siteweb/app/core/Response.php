77988d1dc3dc0624bd98c599a72760847b796f31
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-05-13 22:39:12 +0200

    Create Response.php

---
+++ siteweb/app/core/Response.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Core;
+
+class Response
+{
+    public static function html(string $content, int $status = 200): void
+    {
+        http_response_code($status);
+        header('Content-Type: text/html; charset=utf-8');
+        echo $content;
+    }
+
+    public static function json(array $data, int $status = 200): void
+    {
+        http_response_code($status);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+}
